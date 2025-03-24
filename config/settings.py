ed55ddc73043138bd17ee476f5aecbe6cebc9da6
elbek2327 <zubaydullayev1609@gmail.com>
2025-03-24 23:51:11 +0500

    something

--- config/settings.py
+++ config/settings.py
@@ -213,5 +213,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'zubaydullayev1609@gmail.com'
-EMAIL_HOST_PASSWORD = 'hxyx wvih mqeb ckfs'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
